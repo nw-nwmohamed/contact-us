25934870277eb49192738ea26161a819c8bf2d26
nw-nwmohamed <mohammedhassen524@gmail.com>
2021-10-28 22:23:42 +0300

    nw-nwmohamed

---
+++ js/main.js
@@ -0,0 +1,76 @@
+(function($) {
+
+	skel.breakpoints({
+		xlarge:	'(max-width: 1680px)',
+		large:	'(max-width: 1280px)',
+		medium:	'(max-width: 980px)',
+		small:	'(max-width: 736px)',
+		xsmall:	'(max-width: 480px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body'),
+			$header = $('#header'),
+			$banner = $('#banner');
+
+		 
+
+
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
+
+	  
+
+
+		
+
+
+		
+
+
+		
+	
+
+
+			var $banner = $('#banner');
+
+			if ($banner.length > 0) {
+
+				
+
+
+					if (skel.vars.IEVersion < 12) {
+
+						$window.on('resize', function() {
+
+							var wh = $window.height() * 0.60,
+								bh = $banner.height();
+
+							$banner.css('height', 'auto');
+
+							window.setTimeout(function() {
+
+								if (bh < wh)
+									$banner.css('height', wh + 'px');
+
+							}, 0);
+
+						});
+
+						$window.on('load', function() {
+							$window.triggerHandler('resize');
+						});
+					}
+					$banner.find('.more')
+						.addClass('scrolly');
+			}
+	});
+
+})(jQuery);
